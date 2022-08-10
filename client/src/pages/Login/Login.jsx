af47c66fe930c174a90a3136d4dcd4b6dadf03e1
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2022-08-10 16:23:33 -0300

    add hook login

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,8 +2,25 @@ import * as React from "react";
 import { TextField, Grid, Typography} from "@mui/material";
 import Footer from "../../components/Footer/Footer"
 import ButtonsLogin from "../../components/ButtonsLogin/ButtonsLogin";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(false)
+  const navigate = useNavigate()
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+  const handleClick =  async (e) => {
+    e.preventDefault()
+  }
   return (
     <div>
       <Grid
@@ -23,6 +40,7 @@ const Login = () => {
             label="Email"
             variant="filled"
             fullWidth
+            onChange = {handleEmail}
             style={{ marginBottom: "2em" }}
             sx={{
               input: {
@@ -41,6 +59,7 @@ const Login = () => {
             id="outlined-basic"
             variant="filled"
             style={{ marginBottom: "2em" }}
+            onChange= {handleClick}
             sx={{
               input: {
                 backgroundColor: "#0C0C0C",
