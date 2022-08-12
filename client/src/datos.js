5179639c44f3f5a8bfbf55a425fa63f3a52db971
Valeria Zacarías <vall.zacarias26@gmail.com>
2022-08-12 16:45:31 -0300

    add page post

---
+++ client/src/datos.js
@@ -0,0 +1,101 @@
+export const Users = [
+    {
+      id:1,
+      profilePicture: "assets/person.jpeg",
+      username: "Ezequiel Rodriguez",
+    },
+    {
+      id:2,
+      profilePicture: "assets/person.jpeg",
+      username: "Fiorella Carmagnoli",
+    },
+    {
+      id:3,
+      profilePicture: "assets/person.jpeg",
+      username: "Micaela Padilla",
+    },
+    {
+      id:4,
+      profilePicture: "assets/person.jpeg",
+      username: "Rodrigo Perez",
+    },
+    {
+      id:5,
+      profilePicture: "assets/person.jpeg",
+      username: "Azul Martinez",
+    },
+    {
+      id:6,
+      profilePicture: "assets/person.jpeg",
+      username: "Pedro Ramirez",
+    },
+    {
+      id:7,
+      profilePicture: "assets/person.jpeg",
+      username: "Travis Bennett",
+    },
+  
+  ];
+  
+  export const Posts = [
+    {
+      id: 1,
+      desc: "En la playa.",
+      photo: "assets/image/fondo.jpg",
+      date: "5 mins ago",
+      userId: 1,
+      like: 32,
+      comment: 9,
+    },
+    {
+      id: 2,
+      photo: "assets/fondo.jpg",
+      date: "15 mins ago",
+      userId: 2,
+      like: 2,
+      comment: 1,
+    },
+    {
+      id: 3,
+      desc: "Dia soleado",
+      photo: "assets/fondo.jpg",
+      date: "1 hour ago",
+      userId: 3,
+      like: 61,
+      comment: 2,
+    },
+    {
+      id: 4,
+      photo: "assets/fondo.jpg",
+      date: "4 hours ago",
+      userId: 4,
+      like: 7,
+      comment: 3,
+    },
+    {
+      id: 5,
+      photo: "assets/fondo.jpg",
+      date: "5 hours ago",
+      userId: 5,
+      like: 23,
+      comment: 5,
+    },
+    {
+      id: 6,
+      photo: "assets/fondo.jpg",
+      date: "1 day ago",
+      userId: 6,
+      like: 44,
+      comment: 6,
+    },
+    {
+      id: 7,
+      desc: "Linda aventura.",
+      photo: "assets/fondo.jpg",
+      date: "2 days ago",
+      userId: 7,
+      like: 52,
+      comment: 3,
+    },
+    
+  ];
--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -7,19 +7,13 @@ const Main = () => {
   return (
     <Box>
         <NavBar />
-      <Typography align='center' variant='h3' sx={{ fontWeight:'700'}}>
-        TIP TRAVEL
-      </Typography>
-      <Typography align='center' variant='body2' sx={{ fontWeight: 100, fontFamily: 'Righteous' }}>
-        Red de viajes
-        </Typography>
         <Box sx={{
           backgroundImage: `url(${MainImage})`,
           backgroundRepeat: 'no-repeat',
           backgroundColor: 'black',
           backgroundPosition: 'center',
           backgroundSize: 'cover',
-          height: 800,
+          height: 1000,
           width: '100%',
           display: 'flex',
           justifyContent: 'center',
@@ -27,7 +21,7 @@ const Main = () => {
         >
           <Box sx= {{width: {xs:'100%', sm:'50%', md: '40%'}, padding: {xs:3, sm:2, md:20}}}>
 
-          <Box sx={{background: 'white', opacity:'0.8', fontFamily: 'Righteous'}}>
+          <Box sx={{background: 'white', opacity:'0.7', fontFamily: 'Righteous'}}>
             <Typography variant={'h6'} color= 'red' align='center' pt={8}>
               VIAJAR POR PLACER
             </Typography>
