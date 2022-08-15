f185aaee691ab0fb2a1196fafc4c25a06b4574ce
Pedro Benitez <pedrobenitez2405@gmail.com>
2022-08-15 18:00:11 -0300

    profile me falto la logica de botones

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as React from 'react';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
@@ -9,6 +8,7 @@ import EditIcon from '@mui/icons-material/Edit';
 const Profile = () => {
 
     return (
+        
         <div>
             <img src={User.picture} alt={User.name} />
             <h2> {User.name}</h2>
@@ -16,17 +16,30 @@ const Profile = () => {
         </div>
     )
 }
+const handleClickDelete = () =>{
+
+}
+
+const handleClickSend = () => {
+
+}
+
+const handleClickEdit = () => {
+
+}
+
+
 
 const IconLabelButtons = () => {
     return (
         <Stack direction="row" spacing={2}>
-            <Button variant="outlined" startIcon={<DeleteIcon />}>
+            <Button onClick={handleClickDelete} variant="outlined" startIcon={<DeleteIcon />}>
                 Delete
             </Button>
-            <Button variant="contained" endIcon={<SendIcon />}>
+            <Button onClick={handleClickSend} variant="contained" endIcon={<SendIcon />}>
                 Send
             </Button>
-            <Button variant="contained" endIcon={<EditIcon />}>
+            <Button onClick={handleClickEdit} variant="contained" endIcon={<EditIcon />}>
                 Edit
             </Button>
         </Stack>
