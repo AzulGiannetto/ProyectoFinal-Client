a26270371816c6cceb5a78d0b8348a1caaa818f4
Pedro Benitez <pedrobenitez2405@gmail.com>
2022-08-15 00:28:48 -0300

    profile

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,83 +1,36 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import DeleteIcon from '@material-ui/icons/Delete';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import Icon from '@material-ui/core/Icon';
-import SaveIcon from '@material-ui/icons/Save'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+import EditIcon from '@mui/icons-material/Edit';
 
-const Profile = () =>{ 
+const Profile = () => {
 
     return (
         <div>
-            <img src= {User.picture} alt= {User.name} />
+            <img src={User.picture} alt={User.name} />
             <h2> {User.name}</h2>
             <p>Email {User.email}</p>
         </div>
     )
 }
 
-
-
-
-
-
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}));
-
 const IconLabelButtons = () => {
-  const classes = useStyles();
+    return (
+        <Stack direction="row" spacing={2}>
+            <Button variant="outlined" startIcon={<DeleteIcon />}>
+                Delete
+            </Button>
+            <Button variant="contained" endIcon={<SendIcon />}>
+                Send
+            </Button>
+            <Button variant="contained" endIcon={<EditIcon />}>
+                Edit
+            </Button>
+        </Stack>
+    );
+}
 
-  return (
-    <div>
-      <Button
-        variant="contained"
-        color="secondary"
-        className={classes.button}
-        startIcon={<DeleteIcon />}
-      >
-        Delete
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        endIcon={<Icon>send</Icon>}
-      >
-        Send
-      </Button>
-      <Button
-        variant="contained"
-        color="default"
-        className={classes.button}
-        startIcon={<CloudUploadIcon />}
-      >
-        Upload
-      </Button>
-      <Button
-        variant="contained"
-        disabled
-        color="secondary"
-        className={classes.button}
-        startIcon={<KeyboardVoiceIcon />}
-      >
-      
-        Save
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        className={classes.button}
-        startIcon={<SaveIcon />}
-      >
-        Save
-      </Button>
-    </div>
-  );
-};
-
-export default (Profile,IconLabelButtons);
+export default (Profile, IconLabelButtons);
